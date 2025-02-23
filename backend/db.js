a775e1d07b2859db8711647ed4bc6b8e19baaf5c
hpaliwal8 <hpaliwal@enphaseenergy.com>
2025-02-23 11:56:30 +0530

    For debugging

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoURI = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_USERPWD}@inotebookcluster.gmlgi.mongodb.net/inotebook?retryWrites=true&w=majority`;
+console.log(mongoURI);
 
 const connectToMongo = async() => {
     await mongoose.connect(mongoURI, () => {
